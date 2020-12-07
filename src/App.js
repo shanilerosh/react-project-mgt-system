bba9731af8d7337748847969ec7af8f177516bf9
Shanil Miranda <shanilmiranda24@gmail.com>
2020-12-07 13:42:40 +0530

    Completed signin out and create project uis

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
 import NavBar from "./components/layout/NavBar";
+import CreateProject from "./components/project/CreateProject";
 import ProjectDetail from "./components/project/ProjectDetail";
 
 class App extends Component {
@@ -12,6 +15,9 @@ class App extends Component {
       <Switch>
         <Route exact path='/' component={Dashboard} />
         <Route path='/project/:id' component={ProjectDetail} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/createproject' component={CreateProject} />
       </Switch>
       </BrowserRouter>  
     )
